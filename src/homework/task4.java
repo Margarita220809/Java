d9001ec23eb7a278affb80a5fc468f9c4bb3b9a8
Margarita <margaret220809@gmail.com>
2023-05-03 20:47:28 +0200

    Commit changes task4

--- src/homework/task4.java
+++ src/homework/task4.java
@@ -5,24 +5,25 @@ public class task4 {
         boolean c = true;
 
         String string = "Testing, is my favourite job";
-        String string0 = string.split(" ")[0];
-        String string1 = string.split(" ")[1];
-        String string2 = string.split(" ")[2];
-        String string3 = string.split(" ")[3];
-        String string4 = string.split(" ")[4];
+        String[] words = string.split(" ");
+        String string0 = words[0].replace("," , "");
+        String string1 = words[1];
+        String string2 = words[2];
+        String string3 = words[3];
+        String string4 = words[4];
 
         int a = string1.length();
         int b = string2.length();
         int e = string3.length();
         int d = string4.length();
 
-        System.out.println("Cлово1 = "+string0 +" , Длина этого слова = " + string0.length());
+        System.out.println("Cлово1 = "+string0 +", Длина этого слова = " + string0.length());
         System.out.println("Cлово2 = "+string1 +", Длина этого слова = " + a);
         System.out.println("Cлово3 = "+string2 +", Длина этого слова = " + b);
         System.out.println("Cлово4 = "+string3 +", Длина этого слова = " + e);
         System.out.println("Cлово5 = "+string4 +", Длина этого слова = " + d);
 
-        if (string0.length()> e)
+        if (string0.length()> a && string0.length()> b && string0.length()> e && string0.length()> d)
             System.out.println(c);
             else
                 System.out.println(!c);
